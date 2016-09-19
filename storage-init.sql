ec5c639fca563d6253bbfb1c01b3882544373723
Igor Pavlenko <ipavlenko@inbox.ru>
2016-09-19 11:42:09 +0500

    Big Refactoring

--- storage-init.sql
+++ storage-init.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE sochi_dist DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE USER 'sochi'@'localhost' IDENTIFIED BY 'sochi';
-CREATE USER 'sochi'@'%' IDENTIFIED BY 'sochi';
-GRANT ALL ON `sochi\_%`.* TO 'sochi'@'localhost';
-GRANT ALL ON `sochi\_%`.* TO 'sochi'@'%';
+CREATE DATABASE vstore_new_dist DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+CREATE USER 'vstore'@'localhost' IDENTIFIED BY 'vstore';
+CREATE USER 'vstore'@'%' IDENTIFIED BY 'vstore';
+GRANT ALL ON `vstore\_%`.* TO 'vstore'@'localhost';
+GRANT ALL ON `vstore\_%`.* TO 'vstore'@'%';
